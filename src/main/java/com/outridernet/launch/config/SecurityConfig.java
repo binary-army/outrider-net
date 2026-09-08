98d56f57aa938f1d2b524047af55167067a349db
DareBug <swayamboob96@gmail.com>
2026-09-09 00:32:43 +0530

    WebSocket fix

--- src/main/java/com/outridernet/launch/config/SecurityConfig.java
+++ src/main/java/com/outridernet/launch/config/SecurityConfig.java
@@ -37,7 +37,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/test/**"
                         ).permitAll()
 
                         .requestMatchers(
